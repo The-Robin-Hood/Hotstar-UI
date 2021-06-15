b57e173709a2b2df0058c5be60940c61129081af
Robinhood <ansarithameem674@gmail.com>
2021-06-15 20:02:24 +0530

    initial Commit
    Base Layout has been Created

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,320 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swipper/flutter_card_swiper.dart';
+import 'package:hotstar_ui/Config.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(Hotstar());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Hotstar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: Colors.black12,
+        drawer: new Drawer(
+          elevation: 10,
+          child: Container(
+            color: Colors.black,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 35, horizontal: 10.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          foregroundColor: Colors.black,
+                          child: Icon(
+                            Icons.account_circle,
+                            size: 35,
+                          )),
+                      Text(
+                        "Log in \nFor better Experience",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: Colors.white,
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(right: 5),
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(top: 8),
+                        child: GestureDetector(
+                          onTap: () {
+                            scaffoldKey.currentState!.openDrawer();
+                          },
+                          child: Icon(Icons.menu, color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Image(
+                        image: AssetImage("images/logo.png"),
+                        width: 125,
+                      ),
+                      TextButton(
+                          onPressed: () {},
+                          child: Container(
+                            padding: EdgeInsets.only(top: 13),
+                            child: Container(
+                              padding: EdgeInsets.all(3),
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.blue),
+                                  borderRadius: BorderRadius.circular(3)),
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "SUBSCRIBE",
+                                    style: TextStyle(fontSize: 9),
+                                  ),
+                                  SizedBox(
+                                    width: 3,
+                                  ),
+                                  Icon(
+                                    Icons.arrow_forward_ios,
+                                    size: 10,
+                                  )
+                                ],
+                              ),
+                            ),
+                          )),
+                      SizedBox(
+                        width: 45,
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 8),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.white30,
+                  indent: 25,
+                  endIndent: 25,
+                ),
+                Container(
+                  height: 200,
+                  child: Swiper(
+                    autoplay: true,
+                    itemCount: Poster.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(3),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://image.tmdb.org/t/p/w500/${Poster[Shows[index]]}"),
+                              fit: BoxFit.fill),
+                        ),
+                        margin: EdgeInsets.only(right: 10),
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              bottom: 15,
+                              left: 20,
+                              child: Text(
+                                Shows[index],
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Positioned(
+                              right: 10,
+                              top: 10,
+                              child: Text(
+                                "${index + 1}/5",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  "  Latest & Trending",
+                  style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  height: 150,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      physics: BouncingScrollPhysics(),
+                      itemCount: trending.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Image(
+                            image: NetworkImage(
+                                "https://image.tmdb.org/t/p/w500/${trending[index]}"),
+                            fit: BoxFit.fill,
+                          ),
+                        );
+                      }),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  "  Recommended for you",
+                  style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  height: 150,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      physics: BouncingScrollPhysics(),
+                      itemCount: recommended.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Image(
+                            image: NetworkImage(
+                                "https://image.tmdb.org/t/p/w500/${recommended[index]}"),
+                            fit: BoxFit.fill,
+                          ),
+                        );
+                      }),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  "  Popular Shows",
+                  style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  height: 150,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      physics: BouncingScrollPhysics(),
+                      itemCount: popularShows.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Image(
+                            image: NetworkImage(
+                                "https://image.tmdb.org/t/p/w500/${popularShows[index]}"),
+                            fit: BoxFit.fill,
+                          ),
+                        );
+                      }),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  "  Popular Movies",
+                  style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Container(
+                  height: 150,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      physics: BouncingScrollPhysics(),
+                      itemCount: popularMovies.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.only(right: 10),
+                          child: Image(
+                            image: NetworkImage(
+                                "https://image.tmdb.org/t/p/w500/${popularMovies[index]}"),
+                            fit: BoxFit.fill,
+                          ),
+                        );
+                      }),
+                )
+              ],
             ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.black87,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white38,
+          currentIndex: 0,
+          iconSize: 15,
+          // selectedFontSize: 10,
+          // unselectedFontSize: 10,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.tv), label: "TV"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.sports_cricket), label: "Sports"),
+            BottomNavigationBarItem(icon: Icon(Icons.list_alt), label: "News"),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
